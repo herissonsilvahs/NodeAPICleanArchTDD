3fd84185747bde7783865f0716e983ab061b5dc8
herissonsilvahs <herissonsilvahs@gmail.com>
2020-01-28 23:30:19 -0300

    feature: test if some implementation error occurred

--- src/__tests__/routes/login-router.spec.js
+++ src/__tests__/routes/login-router.spec.js
@@ -1,8 +1,19 @@
 const LoginRouter = {
   route: (httpRequest) => {
-    const { email, password } = httpRequest.body
-    if (!email || !password) {
-      return { status: 400 }
+    try {
+      const { email, password } = httpRequest.body
+      if (!email || !password) {
+        return { status: 400 }
+      }
+    } catch (err) {
+      const objError = {
+        error: {
+          errorName: err.name,
+          errorMessage: err.message
+        }
+      }
+      console.log('Error: ', objError)
+      return { status: 500, message: 'Internal server error' }
     }
   }
 }
@@ -29,5 +40,11 @@ describe('Login router', () => {
 
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.status).toBe(400)
+  })
+
+  test('Should return 500 if some implementation error occurred', () => {
+    const sut = LoginRouter
+    const httpResponse = sut.route()
+    expect(httpResponse.status).toBe(500)
   })
 })
